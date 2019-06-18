31e5d42559c7b1b2b69cc8ff0850d24498466fe0
Hannah0124 <hannahjoo24@gmail.com>
2019-06-18 16:03:38 -0700

    Edit grocery items

--- src/app/grocery/grocery.component.ts
+++ src/app/grocery/grocery.component.ts
@@ -8,7 +8,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GroceryComponent implements OnInit {
   // Define <task> variable. 
-  task: string;
+  // task: string;
+  task = {
+    name: '',
+    id: 0
+  }
 
   // Add <tasks> to keep a collection of tasks entered.
   tasks = [];
@@ -19,9 +23,26 @@ export class GroceryComponent implements OnInit {
   }
 
   // Create <onClick> function.
+  // Check for the id to see if it's an existing item or a new item.
   onClick() {
-    this.tasks.push({name: this.task});
-    this.task = '';
+    if (this.task.id === 0) {
+      // Assign a unique time stamp as id to each task.
+      this.tasks.push({id: (new Date()).getTime(), name: this.task.name})
+    }
+
+    this.task = {
+      name: '',
+      id: 0
+    }
+
+
+    // this.tasks.push({name: this.task});
+    // this.task = '';
+  }
+
+  // Create <onEdit> function.
+  onEdit(item) {
+    this.task = item;
   }
 
 }
